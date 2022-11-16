632496888eb3e379de3b56a3605e3602d06b7184
michaelouru-sw <michaelouru2@gmail.com>
2022-11-16 07:45:35 +0300

    animation function update

--- game.js
+++ game.js
@@ -1,20 +1,27 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var randomNumber = Math.floor(Math.random() * 3) + 1;
+var randomChosenColour = buttonColours[randomNumber];
 
-    $(".btn").click(function () {
-        var userChosenColour = this.id;
-        userClickedPattern.push(userChosenColour);
-    })
+$(".btn").click(function () {
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+})
 
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 3) + 1;
-    var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-
     $("." + randomChosenColour).fadeOut(100).fadeIn(100);
+}
+
+function playSound(name) {
     var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
     audio.play();
+}
 
-
+function animatePress(currentColour) {
+    $(currentColour).addClass("pressed");
+    setTimeout(() => {
+        $(currentColour).removeClass("pressed");
+    }, 100);
 }
